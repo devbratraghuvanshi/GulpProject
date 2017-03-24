4608b523e5efa091e8ef7b8f0e92599bdbf237b5
devbratraghuvanshi <devbratcs0017@gmail.com>
2017-03-25 01:18:45 +0530

    onmy compass is remaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,38 @@
 ///
 //required
 
-var gulp  =require('gulp');
-uglify =require('gulp-uglify');
-rename = require('gulp-rename');
+var gulp  =require('gulp'),
+uglify =require('gulp-uglify'),
+rename = require('gulp-rename'),
+browserSync = require('browser-sync'),
+reload = browserSync.reload;
 
 ///scripts task
 gulp.task('scripts',function(){
-
 	///console.log('it worked dude!');
 	gulp.src(['App/js/**/*.js','!App/js/**/*.min.js'])
 	.pipe(rename({suffix:'.min'}))
 	.pipe(uglify())
 	.pipe(gulp.dest('App/js'));
-
-
 })
+
+///scripts task
+gulp.task('css',function(){
+	///console.log('it worked dude!');
+	gulp.src(['App/css/**/*.css','!App/css/**/*.min.css'])
+})
+
+gulp.task('html',function(){
+	gulp.src('App/**/*.html');
+})
+
+
+
 ///		Watch task
 gulp.task('watch',function(){
-	gulp.watch('App/js/**/*.js',['scripts'])
+	gulp.watch('App/js/**/*.js',['scripts']);
+	gulp.watch('App/**/*.html',['html']);
+	gulp.watch('App/**/*.css',['css']);
 })
 
 
