42e2be0576d254f39cfea2a4c619c7a0400fa52b
devbratraghuvanshi <devbratcs0017@gmail.com>
2017-03-25 01:23:12 +0530

    adding css and js dir

---
+++ App/js/main.js
@@ -0,0 +1,6 @@
+console.log('Grate job it worked');
+console.log('Grate job it worked');
+console.log('Grate job it worked');
+console.log('Grate job it worked');
+console.log('Grate job it worked');
+console.log('Fuck Off');
